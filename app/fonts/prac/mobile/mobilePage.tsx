55cca93d3eb4ac9a4dad3c9af9fcda0563ea2659
KDY <govlehdud@naver.com>
2024-11-19 17:07:36 +0900

    commit tailwind End

---
+++ app/fonts/prac/mobile/mobilePage.tsx
@@ -0,0 +1,95 @@
+<div className="flex h-screen items-center justify-center bg-orange-500 text-black">
+  <div className="h-4/5 max-h-[90vh] w-80 max-w-[400px] overflow-auto rounded-2xl bg-white px-5 pt-12 ring-8 ring-black">
+    <span className="text_color flex font-bold">
+      March 2021
+      <svg
+        data-slot="icon"
+        fill="none"
+        stroke-width="1.5"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        width="24"
+        height="24"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m19.5 8.25-7.5 7.5-7.5-7.5"
+        ></path>
+      </svg>
+    </span>
+
+    <div className="mt-2 h-0.5 w-full items-center justify-center bg-black">
+      <div className="pt-5 text-center font-semibold">
+        Hello, Brunno!
+        <br />
+        Your medicines for today
+      </div>
+      <div className="container mt-8 flex justify-center pb-[6.5rem] text-center">
+        <div className="weekly">
+          <div className="day">16</div>
+          <div className="month">Mon</div>
+        </div>
+        <div className="weekly">
+          <div className="day">17</div>
+          <div className="month">Tue</div>
+        </div>
+        <div className="weekly">
+          <div className="day">18</div>
+          <div className="month">Wed</div>
+        </div>
+        <div className="weekly">
+          <div className="day">19</div>
+          <div className="month">Thu</div>
+        </div>
+        <div className="weekly">
+          <div className="day">20</div>
+          <div className="month">Fri</div>
+        </div>
+        <div className="weekly">
+          <div className="day">21</div>
+          <div className="month">Sat</div>
+        </div>
+      </div>
+      <div className="my-1 flex h-[180px] w-full items-center rounded-xl bg-[#2da6ff]">
+        <div className="flex h-[180px] w-5/6 flex-col justify-between rounded-xl bg-[#f9dd70] p-3 font-semibold">
+          <div>
+            <div className="text-xl">Nora-BE</div>
+            <div className="text-[10px]">Norenthindrone - 0.35mg</div>
+          </div>
+          <div className="text-[10px] font-semibold">7h30AM</div>
+        </div>
+        <div className="pl-3">
+          <svg
+            data-slot="icon"
+            fill="none"
+            stroke-width="1.5"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            width="24"
+            height="24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m4.5 12.75 6 6 9-13.5"
+            ></path>
+          </svg>
+        </div>
+      </div>
+
+      <div className="h-[135px] w-full rounded-xl bg-[#68ca8f] p-3">
+        <div>
+          <div className="text-xl font-semibold">Feosol</div>
+          <div className="text-[10px] font-semibold">
+            Ferrous Sulfate - 600mg
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>;
